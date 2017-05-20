521f58a50a355e779e6e16932187ffc0f5feda60
Carl Shiles <brenan@bot.shakti.local>
2017-05-20 15:24:27 -0500

    0.2 tagging alon

--- scripts/translate.sh
+++ scripts/translate.sh
@@ -1,7 +1,11 @@
-#translator='/home/ec2-user/webAML/Community'
-translator=$2
 for file in $1/aml/*.aml
 do
     base=`basename $file .aml`
-    $translator/bin/go "$file" > $1/pages/"$base".json
+    output=$(./translator/bin/go "$file")
+    if [[ $output == j* ]]
+    then 
+	echo "${output:1:${#output}-1}" > $1/pages/"$base".json
+    else
+	echo "${output:1:${#output}-1}" > $1/pages/"$base".html
+    fi
 done
